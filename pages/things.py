e5eab3e9d76d51dfed64b81f2030179c295ecc7e
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2023-12-14 20:08:21 +0000

    commit

--- pages/things.py
+++ pages/things.py
@@ -5,7 +5,11 @@ st.header('encouraging messages')
 rating = st.slider("how are you feeling today?", 1, 5, 1)
 
 if st.button('yeah this feels about right'):
-    if rating == 3:
+    if rating == 1:
+        st.write("tomorrow will be a new day")
+    elif rating == 2:
+        st.write("better 'tude better time")
+    elif rating == 3:
         st.write("it's a great day to have a great day")
-
-        
+    elif rating == 5:
+        st.write("great days")
