a2059ee42cce6fd920f20eefa3115857edb32147
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2023-12-15 22:08:12 +0000

    commit

--- pages/stuff.py
+++ pages/stuff.py
@@ -4,10 +4,19 @@ import pandas as pd
 def stand_form():
     st.subheader("LP in standard form")
     standard_form = obj_edf
-    if obj_edf.iloc[0,0] == max:
-        standard_form.iloc[0,1] = -1*obj_edf[0,1]
-    elif obj_edf.iloc[0,0] != min:
-        st.write("objective value needs to be min or max")
+    
+    try:
+        if obj_edf.iloc[0,0] == "max":
+            standard_form[0,0] = "min"
+            standard_form.iloc[0,1] = -1*obj_edf.iloc[0,1]
+        
+        st.write(standard_form)
+    except:
+        st.write("input error")
+        st.write("objective box should have min or max")
+        st.write("all variable cells and RHS should have numbers")
+        st.write("inequality cell should have <=, >=, or ==")
+
 
 st.header("LP Simplex")
 
@@ -30,6 +39,5 @@ st.write("*all decision variables must be nonnegative")
 SF_button = st.button("Find LP in Standard Form")
 if SF_button:
     stand_form()
-    SF_button = st.button("Find LP in Standard Form", on_click=disable) 
 
 
