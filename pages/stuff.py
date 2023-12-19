fe2db69b4e5635599f4c6360ea53274a44aa583a
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2023-12-19 15:32:30 +0000

    commit

--- pages/stuff.py
+++ pages/stuff.py
@@ -1,33 +1,56 @@
 import streamlit as st
 import pandas as pd
 
-def stand_form():
-    st.subheader("LP in standard form")
-    standard_form = obj_edf
-    
+def standard_form():   
     try:
+        #objective function 
+        st.subheader("LP in standard form")
+        sf_obj = obj_edf
         if obj_edf.iloc[0,0] == "max":
-            standard_form.iloc[0,0] = "min"
-            standard_form.iloc[0,1] = -1*obj_edf.iloc[0,1]
+            sf_obj.iloc[0,0] = "min"
+            for i in range(1,obj_edf.shape[1]):
+                sf_obj.iloc[0,i] = -1*obj_edf.iloc[0,i]
         elif obj_edf.iloc[0,0] != "min":
             raise Exception
+
         
-        st.write(standard_form)
+        #constraints
+        c_index = 1
+        c_name = " "
+        sf_cons = cons_edf
+        for i in range(len(sf_cons)):
+            s_var = [0,0,0]
+            if sf_cons.loc[i,"inequality"] == "<=":
+                c_name = "s"+str(c_index)
+                s_var[i] = 1
+                sf_cons.insert(c_index,c_name,s_var,True)
+                sf_cons.loc[i,"inequality"] = "=="
+            elif sf_cons.loc[i,"inequality"] == ">=":
+                c_name = "s"+str(c_index)
+                s_var[i] = -1
+                sf_cons.insert(c_index,c_name,s_var,True)
+                sf_cons.loc[i,"inequality"] = "=="
+            elif sf_cons.loc[i,"inequality"] != "==":
+                raise Exception
+            
+            c_index = c_index+1          
+        #display tableaus
+        st.write(sf_obj)        
+        st.write(sf_cons)
     except:
        st.markdown(
-           '''***input error***  
+           '''**input error**  
            objective value should be min or max  
-           inequality cells should be >=, <=, or =='''
-       )
+           inequality cells should be >=, <=, or ==  
+           all other cells should be numbers''')
 
+#initial example
 st.header("LP Simplex")
-
 objective = pd.DataFrame(
     [
-    {"obj": "max", "x1": 1}
+    {"obj": "min", "x1": 1}
 ])
 obj_edf = st.data_editor(objective)
-
 constraints = pd.DataFrame(
     [
        {"x1": 1, "inequality": "<=", "RHS": 4},
@@ -38,8 +61,13 @@ constraints = pd.DataFrame(
 cons_edf = st.data_editor(constraints)
 st.write("*all decision variables must be nonnegative")
 
+#standard form
 SF_button = st.button("Find LP in Standard Form")
 if SF_button:
-    stand_form()
+    standard_form()
+    first = st.button("first tableau")
+    if first:
+        st.write("first")
+
 
 
