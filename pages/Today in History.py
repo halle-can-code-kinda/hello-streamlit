7ebd8edb145a2a1a55686a0d394861b3dc5b9d35
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2023-12-21 16:54:48 +0000

    commit

--- pages/Today in History.py
+++ pages/Today in History.py
@@ -1,9 +1,10 @@
 import requests
 import streamlit as st
 
-text = 'roman empire'
-api_url = 'https://api.api-ninjas.com/v1/historicalevents?text={}'.format(text)
-response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
+month = '12'
+day = '21'
+api_url = 'https://api.api-ninjas.com/v1/historicalevents?month={}&&day={}'.format(month,day)
+response = requests.get(api_url, headers={'X-Api-Key': 'VVWkroJxCJDoV5Y85g8zGQ==WO58i3UJbCn1IxvY'})
 if response.status_code == requests.codes.ok:
     st.write(response.text)
 else:
