848c97cc5411bd900004418426070ac1dcafe958
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2023-12-21 16:35:31 +0000

    commit

--- pages/Cocktail Recipes.py
+++ pages/Cocktail Recipes.py
@@ -10,13 +10,13 @@ ingredient = st.text_input("by ingredients ('blackberry, bourbon')")
 name = st.text_input("by cocktail name")
 
 if ingredient:
-    api_url = 'https://api.api-ninjas.com/v1/cocktail?ingredients={}'.format(ingredient)
-    response = requests.get(api_url, headers={'X-Api-Key': 'VVWkroJxCJDoV5Y85g8zGQ==WO58i3UJbCn1IxvY'})
-    try: 
+    try:
+        api_url = 'https://api.api-ninjas.com/v1/cocktail?ingredients={}'.format(ingredient)
+        response = requests.get(api_url, headers={'X-Api-Key': 'VVWkroJxCJDoV5Y85g8zGQ==WO58i3UJbCn1IxvY'})
         cocktail = pd.DataFrame(eval(response.text))
         st.write(cocktail[['name', 'ingredients','instructions']])
     except:
-        st.write("Error: no cocktail recipe found with all those ingredients", response.status_code, response.text)
+        st.write("Error: no cocktail recipe found with all those ingredients")
 
 if name:
     api_url = 'https://api.api-ninjas.com/v1/cocktail?name={}'.format(name)
