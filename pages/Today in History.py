cbe4e333f79027022abc4af80557a62c52330ee2
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2023-12-21 17:05:49 +0000

    commit

--- pages/Today in History.py
+++ pages/Today in History.py
@@ -1,17 +1,16 @@
 import requests
 import streamlit as st
 import pandas as pd
-
-month = '12'
-day = '21'
-api_url = 'https://api.api-ninjas.com/v1/historicalevents?month={}&&day={}'.format(month,day)
+from datetime import datetime
+  
+today = datetime.now()
+  
+api_url = 'https://api.api-ninjas.com/v1/historicalevents?month={}&&day={}'.format(today.month,today.day)
 response = requests.get(api_url, headers={'X-Api-Key': 'VVWkroJxCJDoV5Y85g8zGQ==WO58i3UJbCn1IxvY'})
 if response.status_code == requests.codes.ok:
-    st.write(response.text)
     history = pd.DataFrame(eval(response.text))
-    st.write(history)
     st.subheader("Today in History")
     for i in range(len(history)):
-        st.markdown(history.iloc[i,3])
+        st.write(history.iloc[i,0], ":", history.iloc[i,3])
 else:
     st.write("Error:", response.status_code, response.text)
