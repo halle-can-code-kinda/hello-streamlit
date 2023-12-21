ace62c453df1477387afd2b432478cf91312d820
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2023-12-21 16:55:56 +0000

    commit

--- pages/Today in History.py
+++ pages/Today in History.py
@@ -1,5 +1,6 @@
 import requests
 import streamlit as st
+import pandas as pd
 
 month = '12'
 day = '21'
@@ -7,5 +8,7 @@ api_url = 'https://api.api-ninjas.com/v1/historicalevents?month={}&&day={}'.form
 response = requests.get(api_url, headers={'X-Api-Key': 'VVWkroJxCJDoV5Y85g8zGQ==WO58i3UJbCn1IxvY'})
 if response.status_code == requests.codes.ok:
     st.write(response.text)
+    history = pd.DataFrame(eval(response.text))
+    st.write(history)
 else:
     st.write("Error:", response.status_code, response.text)
