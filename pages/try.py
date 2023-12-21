111b901ebcc7ebf0c20048a923229120163361e8
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2023-12-21 17:38:24 +0000

    commit

--- pages/try.py
+++ pages/try.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 
 name = 'Michael Jordan'
-api_url = 'https://api.api-ninjas.com/v1/celebrity?name={}'.format(name)
+api_url = 'https://api.api-ninjas.com/v1/celebrity?birthday={}'.format('1963-02-17')
 response = requests.get(api_url, headers={'X-Api-Key': 'VVWkroJxCJDoV5Y85g8zGQ==WO58i3UJbCn1IxvY'})
 if response.status_code == requests.codes.ok:
     st.write(response.text)
