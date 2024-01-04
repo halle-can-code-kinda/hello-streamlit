57ea92b5305d4de242ecc1859209be3ce5344e07
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2024-01-04 20:19:42 +0000

    commit

--- pages/stockfish.py
+++ pages/stockfish.py
@@ -6,5 +6,5 @@ engine = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
 
 board = chess.Board("r1bqkbnr/p1pp1ppp/1pn5/4p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR w KQkq - 2 4")
 info = engine.analyse(board, chess.engine.Limit(depth=20))
-st.write("Score:", info["score"])
+
 # Score: PovScore(Mate(+1), WHITE)
