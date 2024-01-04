e91540d36537e8c7d341a60f404b6782b92beede
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2024-01-04 15:37:22 +0000

    commit

--- pages/Chess Board Setup.py
+++ pages/Chess Board Setup.py
@@ -5,31 +5,31 @@ import chess
 import chess.svg as svg
 import base64
 
-def render_svg(svg):
-    """Renders the given svg string."""
+def create_board(svg):
     b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
     html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
     st.write(html, unsafe_allow_html=True)
+
 def standard():
     svg = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
-    render_svg(chess.svg.board(svg))
+    create_board(chess.svg.board(svg))
 
 def fischer_random():
     correct = 0
     while correct == 0:
         #generate random board
-        fischer_board = []
-        board_setup = ["R", "N", "B", "Q", "K", "B", "N", "R"]
+        white_position = []
+        white_pieces = ["R", "N", "B", "Q", "K", "B", "N", "R"]
         for i in range(8):
-            piece = random.choice(board_setup)
-            fischer_board.append(piece)
-            board_setup.remove(piece)
+            piece = random.choice(white_pieces)
+            white_position.append(piece)
+            white_pieces.remove(piece)
 
         #check if opposite color bishops 
         even_bishop = 0
         odd_bishop = 0 
-        for i in range(len(fischer_board)):
-            if fischer_board[i] == "B":
+        for i in range(len(white_position)):
+            if white_position[i] == "B":
                 bishop_position = i
                 if (bishop_position/2).is_integer() == True:
                     even_bishop = 1
@@ -37,38 +37,39 @@ def fischer_random():
                     odd_bishop = 1
 
         #check if king is between rooks
-        rook_position = []
-        king_position = 0
+        rook_check = []
+        king_check = 0
         rook = 0
-        for i in range(len(fischer_board)):
-            if fischer_board[i] == "R":
-                rook_position.append(i)
-            elif fischer_board[i] == "K":
-                king_position = i
+        for i in range(len(white_position)):
+            if white_position[i] == "R":
+                rook_check.append(i)
+            elif white_position[i] == "K":
+                king_check = i
         
-        if rook_position[0] < king_position and rook_position[1] > king_position:
+        if rook_check[0] < king_check and rook_check[1] > king_check:
             rook = 1
 
         #check if board is valid 
         if even_bishop == 1 and odd_bishop == 1 and rook == 1:
             correct = 1
 
-    reverse = []
+    black_position = []
     for i in range(8):
-        reverse.append(fischer_board[i].lower())
+        black_position.append(white_position[i].lower())
 
     #write to svg
     svg = ""
     for i in range(8):
-        svg=svg+reverse[i]
+        svg=svg+black_position[i]
     svg=svg+"/pppppppp/8/8/8/8/PPPPPPPP/"
     for i in range (8):
-        svg=svg+fischer_board[i]
+        svg=svg+white_position[i]
     svg = chess.Board(svg)
-    render_svg(chess.svg.board(svg))
+    create_board(chess.svg.board(svg))
    
-
-st.header("Chess Board Setup Generator")
+def transcedental():
+    st.write("cool")
+st.header("Chess Board Setup")
 variant = st.selectbox("Chess Variant", ("Standard", "Fischer Random"))
 button = st.button("Generate Board")
 if button:
@@ -76,3 +77,5 @@ if button:
         standard()
     elif variant == "Fischer Random":
         fischer_random()
+    elif variant == "Transcedental":
+        transcedental()
