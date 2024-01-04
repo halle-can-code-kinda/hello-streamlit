512cc4a7756ba173c1237418c83085b56df76f22
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2024-01-04 20:27:14 +0000

    commit

--- pages/stockfish.py
+++ pages/stockfish.py
@@ -1,10 +1,4 @@
-import chess
-import chess.engine
+import stockfish
 import streamlit as st
 
-engine = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
-
-board = chess.Board("r1bqkbnr/p1pp1ppp/1pn5/4p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR w KQkq - 2 4")
-info = engine.analyse(board, chess.engine.Limit(depth=20))
-
-# Score: PovScore(Mate(+1), WHITE)
+stockfish.set_fen_position("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2")
