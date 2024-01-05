e67df8cf183917d6d94c1dcedfae9381c4603fad
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2024-01-05 20:56:55 +0000

    commit

--- pages/sheets.py
+++ pages/sheets.py
@@ -2,6 +2,8 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
+st.header("Taylor Swift Playlist")
+st.write(" ")
 col1, col2 = st.columns(2)
 url = "https://docs.google.com/spreadsheets/d/1poo8680VUsK15L5N_vh4N1xW4ygzHY3hlh3R2CoNv3g/edit?usp=sharing"
 conn = st.connection("gsheets", type=GSheetsConnection)
@@ -17,13 +19,18 @@ with col1:
     speed = st.select_slider(" ", values,label_visibility="collapsed",value = 4, format_func=(lambda x:speed_labels[x]))
     mode_labels = ['happy',' ',' ',' ','indifferent',' ',' ',' ','sad']
     mode = st.select_slider(" ", values,label_visibility="collapsed",value = 4, format_func=(lambda x:mode_labels[x]))
+    popularity_labels = ['less known',' ',' ',' ','indifferent',' ',' ',' ','hit']
+    mode = st.select_slider(" ", values,label_visibility="collapsed",value = 4, format_func=(lambda x:popularity_labels[x]))
     t1,t2 = st.columns(2)
     with t1: 
-        remix = st.toggle("Include Remixes")
+        remix = st.toggle("Include Remixes", True)
     with t2:
-        live = st.toggle("Include Live Performances")
-
-    button = st.button("Create Playlist")
+        live = st.toggle("Include Live Performances", True)
+    b1,b2 = st.columns(2)
+    with b1:
+        button = st.button("Create Playlist")
+    with b2:
+        lucky = st.button("I'm feeling lucky")
 
 with col2:
     if button:
