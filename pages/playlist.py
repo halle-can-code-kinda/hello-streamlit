e9b1c9dd747a337468e18723aebc90d561034d1b
halle-can-code-kinda <153779059+halle-can-code-kinda@users.noreply.github.com>
2024-02-08 16:13:48 +0000

    commit

--- pages/playlist.py
+++ pages/playlist.py
@@ -1,7 +1,8 @@
 
 import requests
 import json
-spotify_user_id = 'd84a30e86c93422699bb9be941893e25f'
+#spotify_user_id = 'd84a30e86c93422699bb9be941893e25'
+spotify_user_id = 'ynossnhxdkty7r7f48dfmeknr'
 spotify_token = 'fa7bf51d5c0242058e1737db56270f6f5'
 import streamlit as st
 
